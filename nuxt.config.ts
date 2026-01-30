ba9949945df36afdc7364e96bb9e26c4ec534b4f
Julien <julienbouin31@gmail.com>
2026-01-30 19:56:08 +0100

    add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,9 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap' }
       ],
       script: [
-        { src: 'https://accounts.google.com/gsi/client', async: true }
+        { src: 'https://accounts.google.com/gsi/client', async: true },
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-03LZ279F3K', async: true },
+        { innerHTML: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-03LZ279F3K');" }
       ]
     }
   }
